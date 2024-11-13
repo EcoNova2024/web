2d5894100a37941bd60df3769a30894e43644705
Diamond Rain <91908514+DiamondRain0@users.noreply.github.com>
2024-11-13 20:31:32 +0300

    exclude jwt for now

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,5 @@
 // API İLE YAPILACAK HTTP REQUEST TRAFİĞİ YÖNETİLİR
-import { verifyAndGetSession } from "../session";
 import handleResponse from "./handler/response-handler";
-
 
 type APIParams = {
   method: string;
@@ -13,14 +11,12 @@ type APIParams = {
 };
 
 type APIResponse<T> = {
-  map(arg0: (item: any) => { id: any; name: any; price: any; isNew: any; isDamaged: any; image: any; }): import("./products/models").ProductResponse[] | PromiseLike<import("./products/models").ProductResponse[]>;
   body: T;
   message?: string;
   error?: { code: number; message: string };
 };
 
 export async function apiFetch<T>({
-  // TODO: Response Handler yazılacak
   method,
   endpoint,
   payload,
@@ -29,11 +25,9 @@ export async function apiFetch<T>({
   tags,
 }: APIParams): Promise<APIResponse<T>> {
   try {
-    const session = await verifyAndGetSession();
-
+    // No JWT or session token
     const headers: HeadersInit = {
       ...(contentType && { "Content-Type": contentType }),
-      Authorization: session ? `Bearer ${session}` : "",
     };
 
     const options: RequestInit = {
@@ -45,7 +39,7 @@ export async function apiFetch<T>({
 
     if (payload) {
       let body = null;
-      if (contentType == "application/json") {
+      if (contentType === "application/json") {
         body = JSON.stringify(payload);
       } else {
         body = payload as FormData;
@@ -53,8 +47,8 @@ export async function apiFetch<T>({
       options.body = body;
     }
 
-    console.debug(options);
-    const response = await fetch(`${process.env.API_URL}/${endpoint}`, options);
+    console.debug("Request options:", options);
+    const response = await fetch(`http://13.61.9.142:3000/${endpoint}`, options);
     const data = await handleResponse(response);
     console.debug(
       "Response Data => " +
@@ -62,9 +56,8 @@ export async function apiFetch<T>({
     );
     return data;
   } catch (error) {
-    // Client tarafından request işlemi yapılırken gerçekleşen hatalar
-
-    console.debug("Response error: " + error);
+    // Handle client-side request errors
+    console.error("Request error:", error);
     return {
       body: {} as T,
       error: {
